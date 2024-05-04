d7f0261567aaa79edddaa7f8949f3902b79bd5f0
Fabio Kosta <fabiotenet@yahoo.com.br>
2024-05-04 20:27:41 -0300

    Inicio criação Crud  Cadastro Veiculo

---
+++ src/ManagerMoney/Models/Veiculo.cs
@@ -0,0 +1,22 @@
+﻿namespace ManagerMoney.Models
+{
+    public class Veiculo
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public string Placa { get; set; }
+
+        public string Fabricante { get; set; }
+
+        public string Cor { get; set; }
+
+        public int AnoFabricacao { get; set; }
+
+        public int AnoModelo { get; set; }
+
+
+
+
+    }
+}
