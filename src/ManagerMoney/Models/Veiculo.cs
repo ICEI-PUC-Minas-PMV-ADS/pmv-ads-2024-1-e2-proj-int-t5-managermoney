eb28e3e1288734c33c53d4766dc2faa6d8c2bca7
Fabio Kosta <fabiotenet@yahoo.com.br>
2024-05-04 20:58:06 -0300

    Criação Crud  "Modelo Veículo"

--- src/ManagerMoney/Models/Veiculo.cs
+++ src/ManagerMoney/Models/Veiculo.cs
@@ -1,18 +1,32 @@
-﻿namespace ManagerMoney.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ManagerMoney.Models
 {
+
+    [Table("Veiculos")]
     public class Veiculo
     {
+        [Key]
         public int Id { get; set; }
-        public string Nome { get; set; }
 
+        [Required(ErrorMessage ="Obrigatório informar o nome")]
+        public string Nome { get; set; }
+        
+        [Key]
+        [Required(ErrorMessage = "Obrigatório informar a Placa")]
         public string Placa { get; set; }
 
+        [Required(ErrorMessage = "Obrigatório informar o Fabricante ")]
         public string Fabricante { get; set; }
 
+        [Required(ErrorMessage = "Obrigatório informar a Cor ")]
         public string Cor { get; set; }
 
+        [Required(ErrorMessage = "Obrigatório informar o Ano de Fabricação ")]
         public int AnoFabricacao { get; set; }
 
+        [Required(ErrorMessage = "Obrigatório informar o Ano do Modelo")]
         public int AnoModelo { get; set; }
 
 
