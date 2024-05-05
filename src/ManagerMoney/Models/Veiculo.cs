1abce7b5297d4473f60c80754267a423b9ca7201
Fabio Kosta <fabiotenet@yahoo.com.br>
2024-05-05 02:21:24 -0300

    Foi alterados dados do Crud Veiculo. Apos alteraçao não esta mais gerando  Tabelas.

--- src/ManagerMoney/Models/Veiculo.cs
+++ src/ManagerMoney/Models/Veiculo.cs
@@ -7,13 +7,13 @@ namespace ManagerMoney.Models
     [Table("Veiculos")]
     public class Veiculo
     {
-        
+        [Key]
         public int Id { get; set; }
 
         [Required(ErrorMessage ="Obrigatório informar o nome")]
         public string Nome { get; set; }
         
-        [Key]
+
         [Required(ErrorMessage = "Obrigatório informar a Placa")]
         public string Placa { get; set; }
 
