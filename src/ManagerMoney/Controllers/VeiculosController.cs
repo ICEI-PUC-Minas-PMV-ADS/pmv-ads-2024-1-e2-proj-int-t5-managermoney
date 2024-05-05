9a1bbbbd97488883eec97405f685ce6a874f4caf
Fabio Kosta <fabiotenet@yahoo.com.br>
2024-05-04 22:58:57 -0300

    Configurações Controller

---
+++ src/ManagerMoney/Controllers/VeiculosController.cs
@@ -0,0 +1,21 @@
+﻿using ManagerMoney.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManagerMoney.Controllers
+{
+    public class VeiculosController: Controller
+    {
+        private readonly AppDbContext _context;
+        public VeiculosController(AppDbContext contex)
+        {
+                _context = contex;  
+        }
+
+
+        public IActionResult Index()
+        { return View(); 
+        
+        }  
+
+    }
+}
